dab23a77367580f88c6c4f3249f8782062af7cbd
renushri2010 <90689502+renushri2010@users.noreply.github.com>
2022-07-06 20:42:30 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
+noseX = 0;
+noseY = 0;
+
 function preload()
-{}
+{
+    mustache = loadImage('https://i.postimg.cc/3x3QzSGq/m.png');
+}
 
 function setup()
 {
@@ -24,14 +29,17 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
-        console.log("nose x = " + results[0].pose.nose.x);
-        console.log("nose y = " + results[0].pose.nose.y);
+        noseX = results[0].pose.nose.x - 35;
+        noseY = results[0].pose.nose.y + 15;
+        console.log("nose x = " + noseX);
+        console.log("nose y = " + noseY);
     }
 }
 
 function draw()
 {
     image(video, 0, 0, 300, 300);
+    image(mustache, noseX, noseY, 80, 35);
 }
 
 function take_snapshot()
